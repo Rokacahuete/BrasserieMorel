fa29000852ea21b0c2f06ce448fe49e09286c56a
Roka <brian.baur.pro@gmail.com>
2025-08-17 18:20:16 +0200

    Auto switching events

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function Ready() {
     DropDownMenuSwitch();
     LoadYML();
     UpdateEvent();
-    UpdateBeers();
+    UpdateEventBeers();
 }
 
 let dropDownMenuIsOpen = false;
@@ -21,23 +21,44 @@ function LoadYML() {
 
 // Events
 let AEvents = [];
+let eventIndex = -1;
 function UpdateEvent() {
+    const L_EVENTS_COUNT = config.events.length;
+    if (L_EVENTS_COUNT <= 0) return;
+
     const L_EVENT_TITLE = document.getElementById("event-title");
     const L_EVENT_DATE = document.getElementById("event-date");
     const L_EVENT_IMAGE = document.getElementById("event-img");
 
-    L_EVENT_TITLE.innerHTML = config.events[0].name;
-    L_EVENT_DATE.innerHTML = config.events[0].date;
-    L_EVENT_IMAGE.src = `Images/${config.events[0].img}`;
+    eventIndex = ++eventIndex % L_EVENTS_COUNT;
+    let lEvent = config.events[eventIndex];
+
+    L_EVENT_TITLE.innerHTML = lEvent.name;
+    L_EVENT_DATE.innerHTML = lEvent.date;
+    L_EVENT_IMAGE.src = `Images/${lEvent.img}`;
+
+    console.log(`event ${eventIndex}`);
+
+    setTimeout(UpdateEvent, 2_500);
 }
 
 // Beers
-function UpdateBeers() {
+let beerIndex = -1;
+function UpdateEventBeers() {
+    const L_BEERS_COUNT = config.event_beers.length;
+    if (L_BEERS_COUNT <= 0) return;
+
     const L_EVENT_BEER_NAME = document.getElementById("event-beer-name");
     const L_EVENT_BEER_IMAGE = document.getElementById("event-beer-img");
 
-    L_EVENT_BEER_NAME.innerHTML = config.event_beers[0].name;
-    L_EVENT_BEER_IMAGE.src = `Images/${config.event_beers[0].img}`;
+    beerIndex = ++beerIndex % L_BEERS_COUNT;
+    let lBeer = config.event_beers[beerIndex];
+
+    L_EVENT_BEER_NAME.innerHTML = lBeer.name;
+    L_EVENT_BEER_IMAGE.src = `Images/${lBeer.img}`;
+    console.log(`beer ${beerIndex}`);
+
+    setTimeout(UpdateEventBeers, 2_500);
 }
 
 

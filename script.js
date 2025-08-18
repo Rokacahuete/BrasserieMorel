16a6ba23cc41ff353d8548f6dfa8e5472ef5dc2d
Roka <brian.baur.pro@gmail.com>
2025-08-18 17:08:29 +0200

    Removing events when no event

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 function Ready() {
     DropDownMenuSwitch();
     LoadYML();
-    UpdateEvent();
-    UpdateEventBeers();
+    UpdateEvent(0);
+    UpdateEventBeers(0);
 }
 
 let dropDownMenuIsOpen = false;
@@ -21,44 +21,49 @@ function LoadYML() {
 
 // Events
 let AEvents = [];
-let eventIndex = -1;
-function UpdateEvent() {
-    const L_EVENTS_COUNT = config.events.length;
-    if (L_EVENTS_COUNT <= 0) return;
+let eventIndex = 0;
+let eventTimeout;
+function UpdateEvent(pIndex) {
+    if (!config.events) return document.getElementById("events").remove();
 
+    const L_EVENTS_COUNT = config.events.length;
     const L_EVENT_TITLE = document.getElementById("event-title");
     const L_EVENT_DATE = document.getElementById("event-date");
     const L_EVENT_IMAGE = document.getElementById("event-img");
 
-    eventIndex = ++eventIndex % L_EVENTS_COUNT;
+    eventIndex += pIndex;
+    if (eventIndex < 0) eventIndex = L_EVENTS_COUNT - 1;
+    else if (eventIndex >= L_EVENTS_COUNT) eventIndex = 0;
     let lEvent = config.events[eventIndex];
 
     L_EVENT_TITLE.innerHTML = lEvent.name;
     L_EVENT_DATE.innerHTML = lEvent.date;
     L_EVENT_IMAGE.src = `Images/${lEvent.img}`;
 
-    console.log(`event ${eventIndex}`);
-
-    setTimeout(UpdateEvent, 2_500);
+    if (eventTimeout) clearTimeout(eventTimeout);
+    eventTimeout = setTimeout(() => UpdateEvent(1), 3_500);
 }
 
 // Beers
-let beerIndex = -1;
-function UpdateEventBeers() {
-    const L_BEERS_COUNT = config.event_beers.length;
-    if (L_BEERS_COUNT <= 0) return;
+let beerIndex = 0;
+let beerTimeout;
+function UpdateEventBeers(pIndex) {
+    if (!config.event_beers) return document.getElementById("beer-event").remove();
 
+    const L_BEERS_COUNT = config.event_beers.length;
     const L_EVENT_BEER_NAME = document.getElementById("event-beer-name");
     const L_EVENT_BEER_IMAGE = document.getElementById("event-beer-img");
 
-    beerIndex = ++beerIndex % L_BEERS_COUNT;
+    beerIndex += pIndex;
+    if (beerIndex < 0) beerIndex = L_BEERS_COUNT - 1;
+    else if (beerIndex >= L_BEERS_COUNT) beerIndex = 0;
     let lBeer = config.event_beers[beerIndex];
 
     L_EVENT_BEER_NAME.innerHTML = lBeer.name;
     L_EVENT_BEER_IMAGE.src = `Images/${lBeer.img}`;
-    console.log(`beer ${beerIndex}`);
 
-    setTimeout(UpdateEventBeers, 2_500);
+    if (beerTimeout) clearTimeout(beerTimeout);
+    beerTimeout = setTimeout(() => UpdateEventBeers(1), 3_500);
 }
 
 
